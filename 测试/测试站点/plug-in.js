0d7cfca3e1ab948962d4aaa734f874584bd051f4
李杨 <1711467488@qq.com>
2018-03-11 20:14:55 +0800

    no message

--- 测试/测试站点/plug-in.js
+++ 测试/测试站点/plug-in.js
@@ -4,22 +4,18 @@ if ($) {
     console.log('jQuery ready') 
 }
 
-console.log(n)
-
-$(function () {
-
+function test () {
     // 调用测试方法
     search()
-    
     // 检测输入框的值
     console.log('#input值为:', $('#input').val())
-    
     // 重新赋值
     $('#input').val('这是新的值')
     console.log('#input赋值结束')
-
     // 检测输入框的值
     console.log('#input值为:', $('#input').val())
+}
 
-    $('body').append('<span>span</span>')
+$(function () {
+    test()    
 })
