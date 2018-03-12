3959af0205391e04b6be45e9d8c6ea589f220b83
李杨 <1711467488@qq.com>
2018-03-12 08:44:07 +0800

    no message

--- 测试/测试站点/plug-in.js
+++ 测试/测试站点/plug-in.js
@@ -3,6 +3,44 @@ let x = {
     dom: {}
 }
 
+const readXlsx = file => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      const fixdata = data => {
+        let o = ''
+        let l = 0
+        const w = 10240
+        for (; l < data.byteLength / w; ++l) o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w, l * w + w)))
+        o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w)))
+        return o
+      }
+      const getHeaderRow = sheet => {
+        const headers = []
+        const range = XLSX.utils.decode_range(sheet['!ref'])
+        let C
+        const R = range.s.r
+        for (C = range.s.c; C <= range.e.c; ++C) {
+          var cell = sheet[XLSX.utils.encode_cell({ c: C, r: R })]
+          var hdr = 'UNKNOWN ' + C
+          if (cell && cell.t) hdr = XLSX.utils.format_cell(cell)
+          headers.push(hdr)
+        }
+        return headers
+      }
+      reader.onload = e => {
+        const data = e.target.result
+        const fixedData = fixdata(data)
+        const workbook = XLSX.read(btoa(fixedData), { type: 'base64' })
+        const firstSheetName = workbook.SheetNames[0]
+        const worksheet = workbook.Sheets[firstSheetName]
+        const header = getHeaderRow(worksheet)
+        const results = XLSX.utils.sheet_to_json(worksheet)
+        resolve({header, results})
+      }
+      reader.readAsArrayBuffer(file)
+    })
+}
+
 // 等待
 const sleep = async (time = 0) => {
     return new Promise((resolve, reject) => {
@@ -58,6 +96,7 @@ const domCache = () => {
 }
 // 给页面元素注册事件
 const domRegistMethod = () => {
+    // 面板切换按钮
     x.dom.panelToggleButton.on('click', () => {
         if (x.panelShow) {
             x.dom.panelBody.hide()
@@ -68,6 +107,14 @@ const domRegistMethod = () => {
             x.panelShow = true
             x.dom.panelToggleButton.text('隐藏')
         }
+    })
+    // 打开Excel文件输入框
+    x.dom.upload.on('change', () => {
+        const file = x.dom.upload.get(0).files[0]
+        readXlsx(file)
+            .then(res => {
+                console.log(res)
+            })
     })
 }
 
